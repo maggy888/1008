0e1aaaf0f381f68f516b1ff756f4e40b137a8186
maggyqoo <maggyqoo@gmail.com>
2024-09-27 14:45:36 +0800

    更新程式

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/moon-like/',
+  base: '/https://github.com/maggy888/0927.git/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
