099d1d3140c61bd7f83f65d1fc1a1f29dfb63d67
maggyqoo <maggyqoo@gmail.com>
2024-10-02 21:03:27 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/0927/',
+  base: '/1002/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
